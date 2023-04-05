3bca697a8585d2366693e312d428c6bf5b14cedb
jeremychristiandi <jeremychristiandi89@gmail.com>
2023-04-05 12:23:52 +0700

    deploy4

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const ExpressError = require("./helper/ExpressError");
 const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 // const dbURL = "mongodb://127.0.0.1:27017/yelp-camp";
 
+mongoose.set("strictQuery", false);
 // connection to MongoDB
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
